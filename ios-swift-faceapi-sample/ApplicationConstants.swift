a1a4e64f8357af105c5322d00bd895c75f4a0339
johnaustin <johnau@microsoft.com>
2017-03-20 15:30:15 -0700

    removed client id and subscription from constants

--- ios-swift-faceapi-sample/ApplicationConstants.swift
+++ ios-swift-faceapi-sample/ApplicationConstants.swift
@@ -8,12 +8,12 @@ import Foundation
 struct ApplicationConstants {
     
     // Graph information
-    static let clientId = "231aa189-b46a-416a-9426-1295a8da42d8"
+    static let clientId = "ENTER_CLIENT_ID"
     static let scopes   = ["User.ReadBasic.All",
                            "offline_access"]
     
     // Cognitive services information
-    static let ocpApimSubscriptionKey = "5ce6674319884aff819a8bdf473c3256"
+    static let ocpApimSubscriptionKey = "ENTER_SUBSCRIPTION_KEY"
     static let selectString = "surname,displayName,userPrincipalName"
     static let orderByString = "displayName"
 }
