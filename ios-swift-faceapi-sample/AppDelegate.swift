673fd58871d36528c427ce2204402cc6a4ccfd1b
Sergey Demchenko <sedemche@microsoft.com>
2019-05-03 13:06:49 -0700

    Finish msal integration.

--- ios-swift-faceapi-sample/AppDelegate.swift
+++ ios-swift-faceapi-sample/AppDelegate.swift
@@ -4,6 +4,7 @@
  */
 
 import UIKit
+import MSAL
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -13,5 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool
+    {
+        guard let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String else { return false }
+        
+        return MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: sourceApplication)
     }
 }
